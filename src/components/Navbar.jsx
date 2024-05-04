d9a98cb863e1fbfcd7f381300693c20b0653b9e4
Manav Trivedi <trivedimanav2003@gmail.com>
2024-05-04 22:39:00 +1000

    cleaned up a bit, ready to pull now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 const Navbar = () => {
     return (
         <div className="bg-white p-4">
-          <h1 className="text-3xl text-indigo-900 font-bold">Brewbucks</h1>
+            <div className="flex items-center space-x-3">
+                <p className="text-3xl text-indigo-900 font-bold">BREWBUCKS</p> 
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite"
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: {transform: "translateX(0)"},
+          to: {transform: "translateX(-100%)"},
+        },
+      },
+    },
   },
   plugins: [
     require('daisyui')
