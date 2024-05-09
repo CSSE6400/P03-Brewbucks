b8bb0f3c75425b0025be580beb115ecf725ff083
Manav Trivedi <trivedimanav2003@gmail.com>
2024-05-09 19:19:08 +1000

    Making changes to login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,17 +10,32 @@ const Login = () => {
 
     return (
         <div className="bg-gray-100 p-4 h-screen flex justify-center items-center">
-            <div className="bg-white rounded-lg p-4 w-1/3 h-2/3">
-                <div className="flex-col  space-y-4">
+            <div className="bg-white rounded-2xl p-4 w-1/3 h-2/3">
+                <div className="flex-col space-y-4">
+                    <div>
+                        <p className="text-3xl text-indigo-900 font-bold">BREWBUCKS</p> 
+                    </div>
                     <div className="">
-                        <input type="text" placeholder="Username" class="input input-bordered input-md w-full max-w-xs border-indigo-900 border-2" />
+                        <input type="text" placeholder="Username" class="input input-bordered input-sm w-full max-w-md" />
                     </div>
                     <div>
-                        <input type="text" placeholder="Password" class="input input-bordered input-md w-full max-w-xs border-indigo-900 border-2" />
+                        <input type="text" placeholder="Password" class="input input-bordered input-sm w-full max-w-md" />
                     </div>
-                    <button onClick={routeChange}>
-                        Log In
-                    </button>
+                    <div className="flex-col space-y-0.5">
+                        <button onClick={routeChange} className="btn btn-sm rounded-lg bg-indigo-900 p-2 content-center w-1/3">
+                            <p className="text-white">
+                                Login
+                            </p>
+                        </button> 
+                        <div>
+                        <p className="text-sm">or</p>
+                        </div>
+                        <button onClick={routeChange} className="btn btn-sm rounded-lg bg-indigo-900 p-2 content-center w-1/3">
+                            <p className="text-white">
+                                Sign Up
+                            </p>
+                        </button> 
+                    </div>
                 </div>
             </div>
         </div>
