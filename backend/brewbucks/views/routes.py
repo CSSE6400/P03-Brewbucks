af8b3ef78ed0503354ee96ef69d1db4124f56559
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2024-05-11 02:37:07 +1000

    return code change

--- backend/brewbucks/views/routes.py
+++ backend/brewbucks/views/routes.py
@@ -14,4 +14,4 @@ def create_test_user():
 #     print(user.to_dict())
     db.session.add(user)
     db.session.commit()
-    return jsonify(user.to_dict()), 201
+    return jsonify(user.to_dict()), 200
