237515faba061327e38fab152e7f791e2887dccc
Manav Trivedi <trivedimanav2003@gmail.com>
2024-05-15 15:59:09 +1000

    Added avatar symbol, and dropdown menu for points and sign out

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
+import AvatarMenu from "./AvatarMenu";
+
 const Navbar = () => {
     return (
         <div className="bg-white p-4">
-            <div className="flex items-center space-x-3">
+            <div className="flex items-center space-x-3 justify-between pl-8 pr-8">
                 <p className="text-3xl text-indigo-900 font-bold">BREWBUCKS</p> 
+                <AvatarMenu points={99999}></AvatarMenu>
             </div>
         </div>
     )
