69fc72af09cf84aa70008ee9f32b4f02b91e72bc
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2024-06-02 14:10:08 +1000

    Vite Config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,11 +3,19 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/",
   server: {
+    port: 80,
+    strictPort: true,
+    host: true,
     watch: {
       usePolling: true
     }
   },
+  preview: {
+    port: 80,
+    strictPort: true,
+   },
   plugins: [react({
     include: "**/*.jsx",
   })]
